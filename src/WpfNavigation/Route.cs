dedfbcd50c5d79cf5274241a05ce4fd72e970432
lr <lrmail>
2022-04-11 19:41:54 +0200

    Introduce Route

---
+++ src/WpfNavigation/Route.cs
@@ -0,0 +1,28 @@
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+
+namespace WpfNavigation;
+
+internal class Route
+{
+    public Route(string name, TemplateSettings templateSettings, TargetSettings targetSettings)
+    {
+        var key = new DataTemplateKey(targetSettings.ContentType);
+        var container = XamlReader.Parse(@"
+            <UserControl
+                    xmlns=""http://schemas.microsoft.com/winfx/2006/xaml/presentation""
+                    xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml""
+                    xmlns:local=""clr-namespace=WpfNavigation.UnitTests.Common"">
+                <UserControl.Resources>
+                    <DataTemplate
+                        DataType=""local:SampleViewModel"">
+                            <local:SampleView x:Name=""SampleView"" />
+                    </DataTemplate>    
+                </UserControl.Resources>
+            </UserControl>") as UserControl;
+        var enumerator = container.Resources.Values.GetEnumerator();
+        enumerator.MoveNext();
+        templateSettings.Resources.Add(key, enumerator.Current);
+    }
+}
