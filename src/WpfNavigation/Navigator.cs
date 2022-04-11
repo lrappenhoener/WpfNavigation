46edbe6ed787802a337dc3fa5050a78c6d7ddad9
lr <lrmail>
2022-04-11 19:43:31 +0200

    Refactor adding routes

--- src/WpfNavigation/Navigator.cs
+++ src/WpfNavigation/Navigator.cs
@@ -10,22 +10,7 @@ public class Navigator
 
     public Navigator AddRoute(string name, TemplateSettings templateSettings, TargetSettings targetSettings)
     {
-        var key = new DataTemplateKey(targetSettings.ContentType);
-        var container = XamlReader.Parse(@"
-            <UserControl
-                    xmlns=""http://schemas.microsoft.com/winfx/2006/xaml/presentation""
-                    xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml""
-                    xmlns:local=""clr-namespace=WpfNavigation.UnitTests.Common"">
-                <UserControl.Resources>
-                    <DataTemplate
-                        DataType=""local:SampleViewModel"">
-                            <local:SampleView x:Name=""SampleView"" />
-                    </DataTemplate>    
-                </UserControl.Resources>
-            </UserControl>") as UserControl;
-        var enumerator = container.Resources.Values.GetEnumerator();
-        enumerator.MoveNext();
-        templateSettings.Resources.Add(key, enumerator.Current);
+        _routes.Add(name, new Route(name, templateSettings, targetSettings));
         return this;
     }
 
