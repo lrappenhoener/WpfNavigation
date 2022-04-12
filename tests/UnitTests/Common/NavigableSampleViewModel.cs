4437248040b3b3357f9f7bbbdf87ebaf13f69bf8
lr <lrmail>
2022-04-12 19:20:04 +0200

    Track invocation order

--- tests/UnitTests/Common/NavigableSampleViewModel.cs
+++ tests/UnitTests/Common/NavigableSampleViewModel.cs
@@ -2,27 +2,36 @@ namespace WpfNavigation.UnitTests.Common;
 
 public class NavigableSampleViewModel : INavigable
 {
+    private int _order = 1;
     public bool PreDeactivationInvoked { get; set; }
     public bool PostDeactivationInvoked { get; set; }
     public bool PreActivationInvoked { get; set; }
     public bool PostActivationInvoked { get; set; }
+    public int PreDeactivationOrder { get; set; }
+    public int PostDeactivationOrder { get; set; }
+    public int PreActivationOrder { get; set; }
+    public int PostActivationOrder { get; set; }
     public void PreDeactivation(RoutingContext ctx)
     {
         PreDeactivationInvoked = true;
+        PreDeactivationOrder = _order++;
     }
 
     public void PostDeactivation(RoutingContext ctx)
     {
-        PostDeactivationInvoked = true;;
+        PostDeactivationInvoked = true;
+        PostDeactivationOrder = _order++;
     }
 
     public void PreActivation(RoutingContext ctx)
     {
         PreActivationInvoked = true;
+        PreActivationOrder = _order++;
     }
 
     public void PostActivation(RoutingContext ctx)
     {
         PostActivationInvoked = true;
+        PostActivationOrder = _order++;
     }
 }
