ba30c1f968a32ff46b37dbfe2ec7a04d89ec4ca3
lr <lrmail>
2022-04-12 19:40:02 +0200

    Call INavigable if content implements

--- src/WpfNavigation/Route.cs
+++ src/WpfNavigation/Route.cs
@@ -15,7 +15,15 @@ internal class Route
     public void Execute()
     {
         var content = _provider.Resolve(_targetSettings.ContentType);
+        var newContentNavigable = content as INavigable;
         var target = RouteTargetFinder.Find(_targetSettings.Root, _targetSettings.Uid);
+        var currentContentNavigable = target.Content as INavigable;
+
+        var ctx = new RoutingContext(target.Content, content);
+        currentContentNavigable?.PreDeactivation(ctx);
+        newContentNavigable?.PreActivation(ctx);
         target.Content = content;
+        newContentNavigable?.PostActivation(ctx);
+        currentContentNavigable?.PostDeactivation(ctx);
     }
 }
