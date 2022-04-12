ac72f14c9ae5017230b9275841df479bb0564428
lr <lrmail>
2022-04-12 10:43:16 +0200

    Add Execute to Route

--- src/WpfNavigation/Route.cs
+++ src/WpfNavigation/Route.cs
@@ -1,13 +1,23 @@
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Markup;
+using System.Windows.Media;
 
 namespace WpfNavigation;
 
 internal class Route
 {
+    private readonly string _name;
+    private readonly TemplateSettings _templateSettings;
+    private readonly TargetSettings _targetSettings;
+    private readonly IProvider _provider;
+
     public Route(string name, TemplateSettings templateSettings, TargetSettings targetSettings, IProvider provider)
     {
+        _name = name;
+        _templateSettings = templateSettings;
+        _targetSettings = targetSettings;
+        _provider = provider;
         var key = new DataTemplateKey(targetSettings.ContentType);
         var container = XamlReader.Parse(@"
             <UserControl
@@ -24,5 +34,45 @@ internal class Route
         var enumerator = container.Resources.Values.GetEnumerator();
         enumerator.MoveNext();
         templateSettings.Resources.Add(key, enumerator.Current);
+    }
+
+    public void Execute()
+    {
+        var content = _provider.Resolve(_targetSettings.ContentType);
+        var target = FindTarget();
+        target.Content = content;
+    }
+
+    private ContentControl FindTarget()
+    {
+        var queue = new Queue<UIElement>();
+        queue.Enqueue(_targetSettings.Root);
+        while (queue.Count > 0)
+        {
+            var current = queue.Dequeue();
+            if (current.Uid == _targetSettings.Uid) return current as ContentControl;
+            if (current is Panel panel)
+            {
+                foreach (UIElement child in panel.Children)
+                {
+                    queue.Enqueue(child);
+                }
+            }
+            else if (current is ItemsControl itemsControl)
+            {
+                foreach (var child in itemsControl.Items)
+                {
+                    if (child is UIElement uiChild)
+                        queue.Enqueue(uiChild);
+                }
+            }
+            else if (current is ContentControl contentControl)
+            {
+                if (contentControl.Content is UIElement contentChild)
+                    queue.Enqueue(contentChild);
+            }
+        }
+
+        return null;
     }
 }
